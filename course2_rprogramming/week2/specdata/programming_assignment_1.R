1d523dc8b307a41eb599cf90c41722738898473f
BlackBox712 <psankalp2009@gmail.com>
2020-05-26 16:46:52 +0530

    updated programming_assignment_1.R with 3 functions and quiz codes for q1-q10

--- course2_rprogramming/week2/specdata/programming_assignment_1.R
+++ course2_rprogramming/week2/specdata/programming_assignment_1.R
@@ -1,3 +1,4 @@
+#pollutantmean function (part 1)
 pollutantmean <- function(directory, pollutant, id = 1:332)
 {
     all_files <- list.files(path = directory, full.names = TRUE)
@@ -11,6 +12,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332)
     mean(file_data[, pollutant], na.rm = TRUE)
 }
 
+#complete function (part 2)
 complete <- function(directory, id = 1:332)
 {
     all_files <- list.files(path = directory, full.names = TRUE)
@@ -27,9 +29,12 @@ complete <- function(directory, id = 1:332)
         nobs <- rbind(nobs, nobs_row)
     }
     
+    colnames(nobs) <- c("id", "nobs")
+    
     nobs
 }
 
+#corr function (part 3)
 corr <- function(directory, threshold = 0)
 {
     all_files <- list.files(path = directory, full.names = TRUE)
@@ -54,3 +59,57 @@ corr <- function(directory, threshold = 0)
     
     corrs
 }
+
+
+#quiz
+
+#q1
+pollutantmean("specdata", "sulfate", 1:10)
+
+#q2
+pollutantmean("specdata", "nitrate", 70:72)
+
+#q3
+pollutantmean("specdata", "sulfate", 34)
+
+#q4
+pollutantmean("specdata", "nitrate")
+
+#q5
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+
+#q6
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+#q7
+RNGversion("3.5.1")  
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+#q8
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+#q9
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+#q10
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
