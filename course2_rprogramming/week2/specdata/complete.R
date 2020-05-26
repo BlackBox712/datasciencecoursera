c2ba64b5bb20f2404af4b4e412af9ce2ba5bf064
BlackBox712 <psankalp2009@gmail.com>
2020-05-26 15:01:17 +0530

    updated complete.R

---
+++ course2_rprogramming/week2/specdata/complete.R
@@ -0,0 +1,18 @@
+complete <- function(directory, id = 1:332)
+{
+    all_files <- list.files(path = directory, full.names = TRUE)
+    file_data <- data.frame()
+    
+    nobs <- data.frame()
+    
+    for (i in id) 
+    {
+        file_data <- read.csv(all_files[i])
+        
+        file_nobs <- sum(complete.cases(file_data))
+        nobs_row <- data.frame(i, file_nobs)
+        nobs <- rbind(nobs, nobs_row)
+    }
+    
+    nobs
+}
