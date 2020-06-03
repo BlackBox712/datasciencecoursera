d29f0ae1562c8174b8e415dea0646b97d80859c4
BlackBox712 <psankalp2009@gmail.com>
2020-06-03 20:34:17 +0530

    updated best.R

--- course2_rprogramming/week4/rprog_data_ProgAssignment3-data/best.R
+++ course2_rprogramming/week4/rprog_data_ProgAssignment3-data/best.R
@@ -1,4 +1,36 @@
 best <- function(state, outcome)
 {
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
+    if(!state %in% unique(data[["State"]]))
+    {
+        stop("invalid state")
+    }
+    
+    if(outcome == "heart attack")
+    {
+        col_index <- 11
+    }
+    else if(outcome == "heart failure")
+    {
+        col_index <- 17
+    }
+    else if(outcome == "pneumonia")
+    {
+        col_index <- 23
+    }
+    else
+    {
+        stop("invalid outcome")
+    }
+    
+    state_data <- subset(data, State == state)
+    suppressWarnings(
+        row_index <- which.min(as.numeric(state_data[, col_index]))
+    )
+    
+    hospitals <- state_data[row_index, 2]
+    hospitals <- sort(hospitals)
+    hospitals[1]
     
 }
