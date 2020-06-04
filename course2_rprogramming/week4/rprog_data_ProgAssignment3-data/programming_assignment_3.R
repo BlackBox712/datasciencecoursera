8379e38dbf46d46ccd3faa8c3009a7969c969019
BlackBox712 <psankalp2009@gmail.com>
2020-06-04 13:33:44 +0530

    created programming_assignment_3.R

---
+++ course2_rprogramming/week4/rprog_data_ProgAssignment3-data/programming_assignment_3.R
@@ -0,0 +1,24 @@
+#q1
+best("SC", "heart attack")
+
+#q2
+best("NY", "pneumonia")
+
+#q3
+best("AK", "pneumonia")
+
+#q4
+rankhospital("NC", "heart attack", "worst")
+
+#q5
+rankhospital("WA", "heart attack", 7)
+
+#q6
+rankhospital("TX", "pneumonia", 10)
+
+#q7
+rankhospital("NY", "heart attack", 7)
+
+#q8
+r <- rankall("heart attack", 4)
+subset(r, state == "HI")
