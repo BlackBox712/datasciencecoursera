a00b2803e17cc88a0aefbab75f8197af3558e92b
BlackBox712 <psankalp2009@gmail.com>
2020-06-08 12:56:52 +0530

    updated week1_quiz.R with q1-q2 codes

--- course3_gettingandcleaningdata/week1/week1_quiz.R
+++ course3_gettingandcleaningdata/week1/week1_quiz.R
@@ -1,8 +1,15 @@
 #q1
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileUrl, destfile = "./q1_datafile.csv")
+fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl1, destfile = "./q1_datafile.csv")
 
 q1_data <- read.csv("q1_datafile.csv")
 sum(q1_data$VAL == 24, na.rm = TRUE)
 
 #q2 (same dataset as q1)
+sum(is.na(q1_data$FES)) == 0
+
+#q3
+library("xlsx")
+
+fileUrl2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl2, destfile = "q3_datafile.xlsx")
