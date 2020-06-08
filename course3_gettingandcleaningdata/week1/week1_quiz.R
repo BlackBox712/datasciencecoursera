4fdf5420e0fd5a7003f450bc044393260e1c11bb
BlackBox712 <psankalp2009@gmail.com>
2020-06-08 12:12:43 +0530

    updated week1_quiz.R with q1 code

--- course3_gettingandcleaningdata/week1/week1_quiz.R
+++ course3_gettingandcleaningdata/week1/week1_quiz.R
@@ -1,2 +1,8 @@
+#q1
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileUrl, destfile = "./q1_datafile.csv")
+
+q1_data <- read.csv("q1_datafile.csv")
+sum(q1_data$VAL == 24, na.rm = TRUE)
+
+#q2 (same dataset as q1)
