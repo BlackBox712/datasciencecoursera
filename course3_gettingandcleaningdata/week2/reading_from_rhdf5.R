7ec5d8a343c938ae8c4d5b4edec5aa92e544c325
BlackBox712 <psankalp2009@gmail.com>
2020-06-09 11:27:31 +0530

    updated reading_from_rhdf5.R

--- course3_gettingandcleaningdata/week2/reading_from_rhdf5.R
+++ course3_gettingandcleaningdata/week2/reading_from_rhdf5.R
@@ -2,3 +2,8 @@ library("rhdf5")
 
 created <- h5createFile("example.h5")
 created
+
+created = h5createGroup("example.h5","foo")
+created = h5createGroup("example.h5","baa")
+created = h5createGroup("example.h5","foo/foobaa")
+h5ls("example.h5")
