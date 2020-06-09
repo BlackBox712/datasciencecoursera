65f86e07d1a10078e23faf5f6eb41a1ae23d995e
BlackBox712 <psankalp2009@gmail.com>
2020-06-09 10:42:28 +0530

    updated reading_excel_files.R

--- course3_gettingandcleaningdata/week1/reading_excel_files.R
+++ course3_gettingandcleaningdata/week1/reading_excel_files.R
@@ -1,3 +1,12 @@
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
 download.file(fileUrl, destfile = "./cameras.xlsx")
 dateDownloaded <- date()
+
+library(openxlsx)
+cameraData <- read.xlsx("./cameras.xlsx")
+head(cameraData)
+
+colIndex <- 2:3
+rowIndex <- 1:4
+cameraDataSubset <- read.xlsx("./cameras.xlsx", cols = colIndex,rows = rowIndex)
+cameraDataSubset
