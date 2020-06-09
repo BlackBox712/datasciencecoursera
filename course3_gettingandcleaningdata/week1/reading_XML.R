95a3af9735ea73148580bb8af3b7c102c078a95e
BlackBox712 <psankalp2009@gmail.com>
2020-06-09 10:58:20 +0530

    updated reading_XML.R

---
+++ course3_gettingandcleaningdata/week1/reading_XML.R
@@ -0,0 +1,19 @@
+library(XML)
+fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse("simple.XML", useInternalNodes = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+
+rootNode[[1]]
+rootNode[[1]][[1]]
+
+xmlSApply(rootNode,xmlValue)
+
+xpathSApply(rootNode,"//name",xmlValue)
+xpathSApply(rootNode,"//price",xmlValue)
+
+fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileUrl,useInternal=TRUE)
+scores <- xpathSApply(doc,"//li[@class='score']",xmlValue)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+scores
